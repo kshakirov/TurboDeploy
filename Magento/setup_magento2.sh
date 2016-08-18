26527d39759e7c633ed6f18d8a5975ca76f46b40
kshakirov <kshakirov@zoral.com.ua>
2016-08-18 20:06:33 +0300

    link 2

--- Magento/setup_magento2.sh
+++ Magento/setup_magento2.sh
@@ -1,13 +1,13 @@
 sudo rm -r /var/www
-sudo ln -s ~/TurboInternational /var/www
+sudo ln -s /home/ubuntu/TurboInternational /var/www
 sudo   chown -R www-data /var/www
 cd  /var/wwww
-chown -R www-data .
-find . -type f -exec chmod 400 {} \;
-find . -type d -exec chmod 500 {} \;
-find var/ -type f -exec chmod 600 {} \;
-find media/ -type f -exec chmod 600 {} \;
-find var/ -type d -exec chmod 700 {} \;
-find media/ -type d -exec chmod 700 {} \;
-chmod 700 includes
-chmod 600 includes/config.php
+sudo chown -R www-data .
+sudo find . -type f -exec chmod 400 {} \;
+sudo find . -type d -exec chmod 500 {} \;
+sudo find var/ -type f -exec chmod 600 {} \;
+sudo find media/ -type f -exec chmod 600 {} \;
+sudo find var/ -type d -exec chmod 700 {} \;
+sudo find media/ -type d -exec chmod 700 {} \;
+sudo chmod 700 includes
+sudo chmod 600 includes/config.php
