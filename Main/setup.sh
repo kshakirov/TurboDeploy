5d8dfb493da6840ff9a7410aa9e0fcc0ad65372b
kshakirov <kshakirov@zoral.com.ua>
2016-08-18 20:46:10 +0300

    nginx 3

--- Main/setup.sh
+++ Main/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install git nginx screen mc
-sudo cp /config/default  /etc/nginx/sites-available/
+sudo apt-get install -y git nginx screen mc
+sudo cp config/default  /etc/nginx/sites-available/
 sudo service nginx reload
--- Vagrant/magento_install_script.sh
+++ Vagrant/magento_install_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install -y lxc lxc-dev build-essential ruby-dev
+sudo apt-get install -y lxc lxc-dev build-essential ruby-dev git
 \curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
 echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
  source ~/.bash_profile
