07ce7c8fb210f9d6f55514bfacdd2da4611c5c82
kshakirov <kshakirov@zoral.com.ua>
2016-08-19 15:46:06 +0300

    magento 11

--- Magento/setup_magento_code.sh
+++ Magento/setup_magento_code.sh
@@ -21,5 +21,5 @@ cd ~
 bash < <(wget -q --no-check-certificate -O - https://raw.github.com/colinmollenhour/modman/master/modman-installer)
 cd TurboInternational/magento            # Magento is installed here
 ~/bin/./modman init            # This is only done once in the application root
-~/bin./modman clone https://github.com/colinmollenhour/Cm_RedisSession
-~/bin./modman clone https://github.com/colinmollenhour/Cm_Cache_Backend_Redis
+~/bin/./modman clone https://github.com/colinmollenhour/Cm_RedisSession
+~/bin/./modman clone https://github.com/colinmollenhour/Cm_Cache_Backend_Redis
