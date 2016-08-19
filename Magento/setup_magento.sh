cffcd2a4f54f1047434d49c6409ebf9508f41d51
kshakirov <kshakirov@zoral.com.ua>
2016-08-19 12:08:29 +0300

    magento 6

--- Magento/setup_magento.sh
+++ Magento/setup_magento.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y apache2  php5 php5-mysql php5-curl
-sudo apt-get install -y wget git
+sudo apt-get install -y wget git lynx
 sudo a2enmod rewrite
 sudo apt-get install -y php5-mcrypt
 php5enmod mcrypt
@@ -33,16 +33,19 @@ APACHE'
 
 service apache2 restart
 
-
+cp keys/* ~/.ssh/
 cd ~
 git config --global user.name kshakirov
 git config --global user.email kirill.shakirov4@gmail.com
 git config --global github.user kshakirov
 git config --global github.token 9a5caa991fe9b691efbf840531593d4587567dbb
 git clone git@github.com:pthiry/TurboInternational.git
+cd TurboInternational && git checkout Magento-633-1-angular-4-proxy-home-redis
 
 
 sudo rm -r /var/www
 sudo ln -s /home/ubuntu/TurboInternational/magento /var/www
 sudo   chown -R www-data /var/www
 cd  /var/www &&  sudo chown -R www-data .
+
+service apache2 reload
--- Magento/setup_magento3.sh
+++ Magento/setup_magento3.sh
@@ -1,6 +1,5 @@
-sudo apt-get install -y wget git
+cd ~
 bash < <(wget -q --no-check-certificate -O - https://raw.github.com/colinmollenhour/modman/master/modman-installer)
-bash < <(wget -q --no-check-certificate -O - https://raw.github.com/colinmollenhour/modman/master/modman-installer)
-cd /var/www            # Magento is installed here
+cd TurboInternational            # Magento is installed here
 modman init            # This is only done once in the application root
 modman clone git@example.com:foo/mymodule.git
