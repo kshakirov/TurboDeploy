88df422f27e14124e40202e0c7138403b13ea7ae
kshakirov <kshakirov@zoral.com.ua>
2016-08-19 17:37:31 +0300

    ready to amazon

--- Aws/magento_install_script.sh
+++ Aws/magento_install_script.sh
@@ -1,20 +1,14 @@
-#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y lxc lxc-dev build-essential ruby-dev git
 \curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
 echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
  source ~/.bash_profile
 rvm install ruby
-sudo apt-get install lxc
-apt-get install lxc lxc-dev build-essential ruby-dev
-gem install chef-lxc
-curl -L https://www.opscode.com/chef/install.sh | sudo  bash
-
-#################### chef repo #################################
-wget http://github.com/opscode/chef-repo/tarball/master
-mv chef-* chef-repo
-rm master
-mkdir ~/.chef
-
-
-########################## creating containers #####################
 
 sudo lxc-create -n redis -t ubuntu
+sudo lxc-create -n elastic -t ubuntu
+sudo lxc-create -n mysql -t ubuntu
+sudo lxc-create -n magento -t ubuntu
+sudo lxc-create -n ruby-proxy -t ubuntu
+
+sudo apt-get install -y  nginx mc screen lynx
--- Mysql/mysql_setup.sh
+++ Mysql/mysql_setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get install -y  mysql-server-5.6
 mysql -u root -pservantes -e "CREATE DATABASE IF NOT EXISTS magento;"
-mysql -u root -pservantes -e "GRANT ALL PRIVILEGES ON magento.* TO magento@'magento' IDENTIFIED BY 'magento'; FLUSH PRIVILEGES;"
+mysql -u root -pservantes -e "GRANT ALL PRIVILEGES ON magento.* TO magento@'%' IDENTIFIED BY 'magento'; FLUSH PRIVILEGES;"
+
