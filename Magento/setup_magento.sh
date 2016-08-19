4e8578b5966ef9102abdb21c2e197747eed356d7
kshakirov <kshakirov@zoral.com.ua>
2016-08-19 11:37:51 +0300

    magento 4

--- Magento/setup_magento.sh
+++ Magento/setup_magento.sh
@@ -1,15 +1,14 @@
 sudo apt-get install -y apache2  php5 php5-mysql php5-curl
+sudo apt-get install -y wget git
 sudo a2enmod rewrite
 sudo apt-get install -y php5-mcrypt
 php5enmod mcrypt
 sudo apt-get install -y php-pear libyaml-dev
-sudo apt-get install  -y php5-dev
+sudo apt-get install  -y php5-dev screen mc
 pecl install yaml
-
 pecl install redis
 echo "extension=redis.so" > /etc/php5/mods-available/redis.ini
 php5enmod redis
-service apache2 restart
 
 sudo bash -c 'cat > /etc/apache2/sites-enabled/000-default.conf <<APACHE
 <VirtualHost *:80>
@@ -30,3 +29,19 @@ sudo bash -c 'cat > /etc/apache2/sites-enabled/000-default.conf <<APACHE
 </VirtualHost>
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
 APACHE'
+
+service apache2 restart
+
+
+cd ~
+git config --global user.name kshakirov
+git config --global user.email kirill.shakirov4@gmail.com
+git config --global github.user kshakirov
+git config --global github.token 9a5caa991fe9b691efbf840531593d4587567dbb
+git clone git@github.com:pthiry/TurboInternational.git
+
+
+sudo rm -r /var/www
+sudo ln -s /home/ubuntu/TurboInternational/magento /var/www
+sudo   chown -R www-data /var/www
+cd  /var/www &&  sudo chown -R www-data .
--- Main/setup.sh
+++ Main/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y git nginx screen mc
+sudo bash -c 'cat > /etc/hosts <<APACHE
+magento  10.0.3.184
+elastic  10.0.3.184
+mysql    10.0.3.184
+APACHE'
 sudo cp config/default  /etc/nginx/sites-available/
 sudo service nginx reload
