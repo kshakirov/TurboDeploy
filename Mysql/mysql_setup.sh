9d5ea0926089759c982fee55e66df0642f945fe8
kshakirov <kshakirov@zoral.com.ua>
2016-08-19 16:58:55 +0300

    redis

--- Mysql/mysql_setup.sh
+++ Mysql/mysql_setup.sh
@@ -1,3 +1,4 @@
-#sudo apt-get install  mysql-server-5.6
+sudo apt-get update
+sudo apt-get install -y  mysql-server-5.6
 mysql -u root -pservantes -e "CREATE DATABASE IF NOT EXISTS magento;"
 mysql -u root -pservantes -e "GRANT ALL PRIVILEGES ON magento.* TO magento@'magento' IDENTIFIED BY 'magento'; FLUSH PRIVILEGES;"
