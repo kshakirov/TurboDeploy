cb8a79b8016c6373955ab9765070010042959232
kshakirov <kshakirov@zoral.com.ua>
2016-08-19 16:35:40 +0300

    magento 15

--- Magento/setup_magento_infrastructure.sh
+++ Magento/setup_magento_infrastructure.sh
@@ -1,14 +1,15 @@
 sudo apt-get update
 sudo apt-get install -y apache2  php5 php5-mysql php5-curl
-sudo apt-get install -y wget git lynx
+sudo apt-get install -y wget git lynx locate
 sudo a2enmod rewrite
 sudo apt-get install -y php5-mcrypt
 sudo php5enmod mcrypt
 sudo apt-get install -y php-pear libyaml-dev
 sudo apt-get install  -y php5-dev screen mc
 sudo pecl install yaml
-sudo pecl install redis
-echo "extension=redis.so" > /etc/php5/mods-available/redis.ini
+sudo pecl install redis-2.2.8
+echo "extension=redis.so" > redis.ini
+sudo cp redis.ini /etc/php5/mods-available/redis.ini
 sudo php5enmod redis
 
 sudo bash -c 'cat > /etc/apache2/sites-enabled/000-default.conf <<APACHE
@@ -30,6 +31,4 @@ sudo bash -c 'cat > /etc/apache2/sites-enabled/000-default.conf <<APACHE
 </VirtualHost>
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
 APACHE'
-
-service apache2 restart
 
